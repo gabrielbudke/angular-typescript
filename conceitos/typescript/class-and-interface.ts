6e812f6fb2dd5453adbb3b0aacf0534de8cf82a2
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2021-11-23 03:50:51 +0000

    :sparkles: Estudando conceitos de typescript

--- conceitos/typescript/class-and-interface.ts
+++ conceitos/typescript/class-and-interface.ts
@@ -1,6 +1,9 @@
 import { Spacecraft } from "./Spacecraft";
 import { MilleniumFalcon, ContainerShip } from "./MilleniumFalcon";
 
+import _ from "lodash";
+console.log(_.pad("Typescript Examples", 40, "="));
+
 // 1. CLASSES
 const ship = new Spacecraft("hyperdrive");
 ship.jumpIntoHyperspace();
